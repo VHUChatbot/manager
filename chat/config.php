9d78d0467954ee0d4671d2fee9bf6e8a8bbe7c37
VHUChatbot <32772149+VHUChatbot@users.noreply.github.com>
2017-10-14 00:54:07 +0700

    Add files via upload

---
+++ chat/config.php
@@ -0,0 +1,10 @@
+<?php
+$DBHOST = "us-cdbr-iron-east-05.cleardb.net"; // Set your database host here
+$DBUSER = "bb615a63828210"; // Set your database user here
+$DBNAME = "heroku_ac1e5be76abebb9"; // Set your database name here
+$DBPW =  "78621080"; // Set your database password here
+$BOT_ID = "59e0f0b0e4b0662e5b005395";
+$TOKEN = "vnbqX6cpvXUXFcOKr5RHJ7psSpHDRzO1hXBY8dkvn50ZkZyWML3YdtoCnKH7FSjC";
+$BLOCK_NAME = "VHU_chat";
+$JSON = "VHU_text";
+?>
