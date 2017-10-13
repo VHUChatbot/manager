f1053eabcf4066f7e1d78461f73047d639f0c96d
VHUChatbot <32772149+VHUChatbot@users.noreply.github.com>
2017-10-14 01:24:48 +0700

    Update config.php

--- chat/config.php
+++ chat/config.php
@@ -1,7 +1,7 @@
 <?php
-$DBHOST = "localhost"; // Set your database host here
-$DBUSER = "id3011658_itcchatbot"; // Set your database user here
-$DBNAME = "id3011658_itc"; // Set your database name here
+$DBHOST = "us-cdbr-iron-east-05.cleardb.net";
+$DBUSER = "bb615a63828210";
+$DBNAME = "heroku_ac1e5be76abebb9";
 $DBPW =  "78621080"; // Set your database password here
 $BOT_ID = "59e0f0b0e4b0662e5b005395";
 $TOKEN = "FA50ZrR0d2hGxpRBQYImVM6psoUdMYE4zgaPwmRPjuDGhCJ7TU1QjB3uDuUMXXnl";
