013c069c26aebbd2ad653433781ddfa8ca481773
VHUChatbot <32772149+VHUChatbot@users.noreply.github.com>
2017-10-14 00:46:24 +0700

    Update config.php

--- chat/config.php
+++ chat/config.php
@@ -4,7 +4,7 @@ $DBUSER = "bb615a63828210"; // Set your database user here
 $DBNAME = "heroku_ac1e5be76abebb9"; // Set your database name here
 $DBPW =  "78621080"; // Set your database password here
 $BOT_ID = "59e0f0b0e4b0662e5b005395";
-$TOKEN = "vnbqX6cpvXUXFcOKr5RHJ7psSpHDRzO1hXBY8dkvn50ZkZyWML3YdtoCnKH7FSjC";
+$TOKEN = "FA50ZrR0d2hGxpRBQYImVM6psoUdMYE4zgaPwmRPjuDGhCJ7TU1QjB3uDuUMXXnl";
 $BLOCK_NAME = "VHU_chat";
 $JSON = "VHU_text";
 ?>
